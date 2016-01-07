ddf14f5a8d6c9ebdba5eb4a4a4c8283d21962be1
jason <374567155@qq.com>
2016-01-08 10:55:29 +1300

    include some data transform and color display method
    transform: linear,log,sqrt,exp,power,normal
    display: hue,hcl,rainbow,topo.colors,cm.colors,bi.polar,heat,gray

--- R/shade.fun.r
+++ R/shade.fun.r
@@ -1,4 +1,4 @@
-shade.map = function(shp,data,colby = 'Populationgrowth',method = 'l',na.fill = 'White',offset = 0.5,col)
+shade.map = function(shp,data,colby = 'Populationgrowth',method = 'linear',display = 'heat',na.fill = 'White',offset = 0 ,col)
 {	
 	###
 	grid.newpage()
@@ -8,7 +8,8 @@ shade.map = function(shp,data,colby = 'Populationgrowth',method = 'l',na.fill = 
 	bbox = shape@bbox
 	xlim = bbox[1,]
 	ylim = bbox[2,]
-	shade.obj = shape.extract(shp = shape,colby = colby, method = method,data = data,na.fill = na.fill,offset = offset,col = col)
+	shade.obj = shape.extract(shp = shape,colby = colby, method = method,data = data,display = display,
+								na.fill = na.fill,offset = offset,col = col)
 	shade.data = shade.obj$polygon
 	shade.id = rownames(shade.data)
 	col = shade.obj$color
@@ -27,7 +28,7 @@ shade.map = function(shp,data,colby = 'Populationgrowth',method = 'l',na.fill = 
 
 
 
-shape.extract = function(shp,colby,method,data,na.fill,offset,col)
+shape.extract = function(shp,colby,method,display,data,na.fill,offset,col)
 {
 	polygon.data = list()
 	j = 0
@@ -51,44 +52,153 @@ shape.extract = function(shp,colby,method,data,na.fill,offset,col)
 	poly.index = rep(1:j,index)
 	latlon = do.call(rbind,polygon.data)
 	rownames(latlon) = poly.index
-	col = col.fun(shp,colby = colby, method = method, each = poly.rep,data = data,na.fill = na.fill,offset = offset,col = col)
+	col = col.fun(shp,colby = colby, method = method,display = display,
+			each = poly.rep,data = data,na.fill = na.fill,offset = offset,col = col)
 	shape.obj = list(polygon = latlon, color = col)
 	shape.obj
 	
 	
 }
 
-
-col.fun = function(shp,data,colby,method,each,na.fill,offset,col)
+##col not include in the function yet
+col.fun = function(shp,data,colby,method,display,each,na.fill,offset,col)
 {
-	if(!missing(data))
-	{
+	##if data is missing then just fill the area randomly ie, display = 'n'
+	if(missing(data))
+	{		
+		orderd.col.trans = each
+		impossible.number = 0.091823021983
+		###fore the method if data is missing
+		display = 'n'
+		warning('the data is missing! use the default display method')
+	}
+	else{
+		##transform method
 		a = data[,colby]
 		b = a - min(a,na.rm = TRUE)
+		if(method =='linear')
+		{	
+			b = b
+		}
+		if(method =='log')
+		{
+			b = log(b + 1)
+			b = b - min(b,na.rm = TRUE)
+		}
+		if(method == 'sqrt')
+		{
+			b = sqrt(b)
+		}
+		if(method == 'exp')
+		{
+			b = exp(b)
+		}
+		if(method == 'power')
+		{
+			b = b^2
+		}
+		if(method == 'normal')
+		{
+			x <<- (b - mean(b,na.rm = TRUE))/sd(b,na.rm = TRUE)
+			b = dnorm(x,0,1)
+		}
 		percent.data = b/diff(range(b,na.rm = TRUE))
 		order = match(shp[[5]],data$Country)
-		orderd.data = percent.data[order]
-		impossible.number = '0.234959823475923487509234875'
-		orderd.col.trans = ifelse(is.na(orderd.data) == TRUE, impossible.number,orderd.data * (1 - offset) + (offset))
-	}else{
-		orderd.col.trans = each
-		impossible.number = '0.234959823475923487509234875'
-		###fore the method if data is missing
-		method = 'n'
+		orderd.data <<- percent.data[order]
+		impossible.number = 0.091823021983
 	}
-	##keep adding method for choose color
-	if(method =='l')
-	{	
+	
+	print(offset)
+	bio.color = c('bi.polar')
+	###the color can not be offset if it is bio-color
+	if(display %in% bio.color)
+	{
+		orderd.col.trans <<- ifelse(is.na(orderd.data) == TRUE, impossible.number,orderd.data)
+	}else
+	{
+		orderd.col.trans <<- ifelse(is.na(orderd.data) == TRUE, impossible.number,orderd.data * (1 - offset) + (offset))
+	}
+	
+	###display method
+	if(display == 'hue')
+	{
 		fill = rgb(1,0,0,orderd.col.trans)
 	}
-	if(method == 'r')
+	if(display == 'hcl')
+	{	
+		fill = hcl(as.numeric(orderd.col.trans)*100,l = 85)
+	}
+	if(display == 'ff')
+	{
+		ab = expand.grid(a = as.numeric(orderd.col.trans)*360,b = 100)
+		Lab = cbind(L =50, ab)
+		srgb = convertColor(Lab, from = "Lab", to = "sRGB")
+		fill = rgb(srgb[, 1], srgb[, 2], srgb[, 3])
+	}
+	if(display == 'heat')
+	{
+		over.col = heat.colors(length(each) * 100)
+		orderd.col = over.col[length(over.col):1]
+		id = round(orderd.col.trans * length(each) * 100)
+		fill = orderd.col[id]
+	}
+	if(display == 'rainbow')
+	{
+		over.col = rainbow(length(each) * 100)
+		orderd.col = over.col[length(over.col):1]
+		id = round(orderd.col.trans * length(each) * 100)
+		fill = orderd.col[id]
+	}
+	if(display == 'terrin.colors')
+	{
+		over.col = terrin.colors(length(each) * 100)
+		orderd.col = over.col[length(over.col):1]
+		id = round(orderd.col.trans * length(each) * 100)
+		fill = orderd.col[id]	
+	}
+	if(display == 'topo.colors')
+	{
+		over.col = topo.colors(length(each) * 100)
+		orderd.col = over.col[length(over.col):1]
+		id = round(orderd.col.trans * length(each) * 100)
+		fill = orderd.col[id]			
+	}
+	if(display == 'cm.colors')
+	{
+		over.col = cm.colors(length(each) * 100)
+		orderd.col = over.col[length(over.col):1]
+		id = round(orderd.col.trans * length(each) * 100)
+		fill = orderd.col[id]				
+	}
+	if(display == 'bi.polar')
+	{
+		col.center = mean(orderd.col.trans)
+		fill = ''
+		re.scale= 1 / max(abs(orderd.col.trans - col.center))
+		alpha = ifelse(orderd.col.trans >= col.center,
+						(orderd.col.trans- col.center) * re.scale,
+						(col.center - orderd.col.trans) * re.scale
+					   )
+		fill = ifelse(orderd.col.trans >= col.center,
+						 rgb(1,0,0,alpha = alpha),
+						 rgb(0,0,1,alpha = alpha)
+					  )
+	}
+	if(display == 'gray')
+	{
+		fill = gray(round(1 - as.numeric(orderd.col.trans),5))
+	}
+	
+	###fill the color randomly notinclude the missing area
+	if(display == 'r')
 	{
 		r = ifelse(is.na(order) == TRUE, impossible.number,runif(order))
 		g = ifelse(is.na(order) == TRUE, impossible.number,runif(order))
 		b = ifelse(is.na(order) == TRUE, impossible.number,runif(order))
 		fill = rgb(r,g,b)	
 	}
-	if(method == 'n')
+	###fill the color randomly does include the missing area
+	if(display == 'n')
 	{
 		r = runif(length(shp@polygons))
 		g = runif(length(shp@polygons))
@@ -96,45 +206,20 @@ col.fun = function(shp,data,colby,method,each,na.fill,offset,col)
 		na.fill = rgb(r,g,b)
 		fill = rgb(r,g,b)	
 	}
-	if(method == 'hue')
-	{	
-		fill = hcl(0,c = as.numeric(orderd.col.trans)*100, l = 60)
-	}
-	if(method == 'ff')
-	{
-		ab = expand.grid(a = as.numeric(orderd.col.trans)*360,b = 100)
-		Lab = cbind(L =50, ab)
-		srgb = convertColor(Lab, from = "Lab", to = "sRGB")
-		fill = rgb(srgb[, 1], srgb[, 2], srgb[, 3])
-	}
-	if(method == 'heat')
-	{
-		a = round(as.numeric(orderd.col.trans) * length(each))
-		fill = heat.colors(length(each))[-a]
-	}
-	if(method == 'rainbow')
-	{
-		a = round(as.numeric(orderd.col.trans) * length(each))
-		fill = rainbow(length(each))[-a]
-	}
-	if(method == 'terrain.colors')
-	{
-		a = round(as.numeric(orderd.col.trans) * length(each))
-		fill = terrain.colors(length(each))[-a]		
-	}
-	if(method == 'topo.colors')
-	{
-		a = round(as.numeric(orderd.col.trans) * length(each))
-		fill = topo.colors(length(each))[-a]			
-	}
-	if(method == 'cm.colors')
-	{
-		a = round(as.numeric(orderd.col.trans) * length(each))
-		fill = cm.colors(length(each))[-a]				
-	}
+	
 	
 	color.each = ifelse(orderd.col.trans== impossible.number, na.fill, fill)
 	color.out = rep(color.each,each)	
 	color.out
 }
 
+function(){
+e = rep(FALSE,length(m.split))
+for(i in 1:length(m.split)){
+	for(j in 1:length(s.split)){
+		e[i] = any(c(any(m.split[[i]] %in% s.split[[j]]),e[i]))
+		
+	}
+}
+missing.name[e]
+}
--- _scripts/wwwworkshett.r
+++ _scripts/wwwworkshett.r
@@ -4,18 +4,77 @@ library(RColorBrewer)
 setwd('C:/Users/yeamin/Downloads/iNZightMaps-dev-integrateplots/data/world')
 data = read.csv('C:/Users/yeamin/Downloads/iNZightMaps-dev-integrateplots/data/Gapminder-2008.csv',skip = 1)
 source('C:/Users/yeamin/Documents/GitHub/iNZightMaps/R/shade.fun.r')
-system.time(shade.map('TM_WORLD_BORDERS-0.3.shp',colby = 'Populationgrowth',data = data,col = 'Yellow'))
+system.time(
+	shade.map('TM_WORLD_BORDERS-0.3.shp',colby = 'IncomePerPerson',data = data,method = 'normal', display = 'heat',offset = 0.1)
+)
 
 
 
 shp.name = 'TM_WORLD_BORDERS-0.3.shp'
 source('C:/Users/yeamin/Documents/GitHub/iNZightMaps/R/shade.fun.r')
-system.time(shade.map(shp.name,colby = 'IncomePerPerson',data = data, method = 'heat',na.fill = 'White',offset = 0))
+system.time(shade.map(shp.name,colby = 'IncomePerPerson',data = data, method = 'normal',na.fill = 'White',offset = 0.2))
 
 source('C:/Users/yeamin/Documents/GitHub/iNZightMaps/R/shade.fun.r')
 shp.name = 'C:/Users/yeamin/Documents/GitHub/iNZightMaps/data/au/map.shp'
 system.time(shade.map(shp.name,colby = 'IncomePerPerson', method = 'n',na.fill = 'White',offset =0.5))
 
 
-shp.name = 'C:/Users/yeamin/Documents/GitHub/iNZightMaps/data/Africa/map.shp'
-system.time(shade.map(shp.name,colby = 'IncomePerPerson', method = 'n',na.fill = 'White',offset =0.5))
+shp.name = 'C:/Users/yeamin/Documents/GitHub/iNZightMaps/data/New Zealand/REGC2013_GV_Clipped.shp'
+system.time(shade.map(shp.name,colby = 'IncomePerPerson', method = 'r',na.fill = 'White',offset =0.5))
+
+offset = 0
+impossible.number = '0.234959823475923487509234875'
+orderd.col.trans = ifelse(is.na(orderd.data) == TRUE, 0.00001,orderd.data * (1 - offset) + (offset))
+
+ss = readShapePoly(shp.name)
+d.name = data$Country
+s.name = as.character(ss@data$NAME)
+
+ii = match(d.name,s.name)
+missing.name = as.character(d.name[is.na(ii)])
+for(i in 1:length(missing.name)
+{
+
+}
+x <- c(as = "asfef", qu = "qwerty", "yuiop[", "b", "stuff.blah.yech")
+# split x on the letter e
+s.split = strsplit(s.name, " |-")
+m.split = strsplit(missing.name," ")
+for(i in 1:length(m.split))
+{
+	for(j in 1:m.split[[i]])
+	{
+		
+	}
+}
+
+match(m.split[[3]], s.split)
+
+lapply(s.split,function(x) which(x == 'United'))
+
+
+
+o = function(x,y) match(x,y)
+mapply(o,a,b)
+
+p = 0
+e = rep(FALSE,length(a))
+for(i in 1:length(a)){
+	for(j in 1:length(b)){
+		e[i] = any(c(any(a[[i]] %in% b[[j]]),e[i]))
+	}
+}
+a = list(c('aa','bb'),'fg','gg','cc')
+b = list('aa',c('aa','ee'),'cc','dd')
+
+####useing
+e = rep(FALSE,length(m.split))
+for(i in 1:length(m.split)){
+	for(j in 1:length(s.split)){
+		e[i] = any(c(any(m.split[[i]] %in% s.split[[j]]),e[i]))
+	}
+}
+
+aa = c(-1,-2,-3,0,1,2,3)
+
+ifelse(aa > 0,
