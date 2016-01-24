a6c13b37371bf6e4416d18196985e45c070eb110
jason <374567155@qq.com>
2016-01-24 22:18:50 +1300

    map.subsetting v1.05
    pre done

--- R/inzshapemap.R
+++ R/inzshapemap.R
@@ -15,85 +15,76 @@ create.inz.shapemapplot = function(obj) {
     col = col.full
     latlon = pf$shape.object$latlon
     #latlon[,1] = lon.rescale(latlon[,1])
-    aa <<- (latlon[,1])
     xlim = range(latlon[,1])
     ylim = range(latlon[,2])
         
     if(pf$full.map == FALSE)
     {
-        ##only the subsetting region(colored.region)
+        ## only the subsetting region(colored.region)
         plot.logic.fill = pf$shape.object$region %in% df$y
         polygon.index.fill = rep(plot.logic.fill,pf$shape.object$col.index)
         points.fill = rep(polygon.index.fill,pf$shape.object$each)
         latlon.fill = latlon[points.fill,]        
-        
         each.fill = pf$shape.object$each[polygon.index.fill]
         col.index.fill = pf$shape.object$col.index[plot.logic.fill]
         col.fill = col.full[polygon.index.fill]
+        
         ## the bbox for latlon.fill
-        xlim.fill = range(latlon.fill[,1])
-        ylim.fill = range(latlon.fill[,2])
+        lim.fill = c(range(latlon.fill[,1]),range(latlon.fill[,2]))
+        with.range.logic = (latlon[,1] > lim.fill[1]) & (latlon[,1] < lim.fill[2]) & 
+                            (latlon[,2] > lim.fill[3]) & (latlon[,2] < lim.fill[4])
 
-        with.range.logic = (latlon[,1] > xlim.fill[1]) & (latlon[,1] < xlim.fill[2]) & 
-                            (latlon[,2] > ylim.fill[1]) & (latlon[,2] < ylim.fill[2])
+        ## matching by using polygons
         match.id = rep(1:length(pf$shape.object$each),pf$shape.object$each)[with.range.logic]
-        big.region = c('Russia','Antarctica')
-       # if(!any(big.region %in% df$y))
-       # {
-        #    print('herer')
-         ##  polygon.index.fill = rep(plot.logic.fill,pf$shape.object$col.index)
-          #  ######stopping
-          #  points.fill = rep(polygon.index.fill,pf$shape.object$each)
-            
-          #  points.fill & match.id
-           # latlon.fill = latlon[!points.fill,]        
-            #xlim.sub = range(latlon.fill[,1])
-            #ylim.sub = range(latlon.fill[,2])
-        #}
-        match.polygon = dd <<- rownames(table(match.id))
-
-        
-        
+        match.polygon = rownames(table(match.id))
         polygon.index.na = (1:length(pf$shape.object$each)) %in% match.polygon
-        
-        
-
-        
-        #print(plot.logic.na)
-        #print(pf$shape.object$col.index)
-        #plot.logic.na = pf$shape.object$region %in% country.na
-        #polygon.index.na = rep(plot.logic.na,pf$shape.object$col.index)
-        #still draw the region/country if the country lies within the map
-        #col.index.na = pf$shape.object$col.index[plot.logic.na]
-
         points.na = rep(polygon.index.na,pf$shape.object$each)
         latlon.na = latlon[points.na,]
         each.na = pf$shape.object$each[polygon.index.na]
         col.na = col.full[polygon.index.na]  
-
+        
+        ## prepare for updating
         latlon.sub = rbind(latlon.fill,latlon.na)
         xlim.sub = range(latlon.sub[,1])
         ylim.sub = range(latlon.sub[,2])
-
-        
-
-
-
-
-
         each.sub = append(each.fill,each.na)
-        #col.index.sub = append(col.index.fill,col.index.na)
         col = append(col.fill,col.na)
+        
+        ## change the limit 
+        country.rep = rep(rep(pf$shape.obj$region,pf$shape.object$col.index),pf$shape.object$each)
+        table.country = table(country.rep[with.range.logic])
+        country.draw = rownames(table.country[table.country > 0])
+        country.na = country.draw[country.draw %in% pf$shape.object$region]
+        big.region = c('Russia','Antarctica')
+        if(any(big.region %in% country.na))
+        {
+            print('something')
+            plot.logic.big = pf$shape.object$region %in% big.region
+            polygon.index.big = rep(plot.logic.big,pf$shape.object$col.index)
+            points.big = rep(polygon.index.big,pf$shape.object$each)
+            points.fill[points.big] = FALSE
+           
+            ## prepare for updating
+            latlon.big = latlon[points.fill,]     
+            xlim.sub = range(latlon.big[,1])
+            ylim.sub = range(latlon.big[,2])
+            
+        }
+        ## may need
+        #col.index.sub = append(col.index.fill,col.index.na)
+        #pf$shape.object$col.index = col.index.sub
         pf$shape.object$latlon = latlon.sub 
         pf$shape.object$each = each.sub
-        #pf$shape.object$col.index = col.index.sub
         xlim = xlim.sub
         ylim = ylim.sub
     }
+    
 
-    ## cols = col.fun(pf$shape.object$col.fun, pf$shape.object$col.args)
     latlon = pf$shape.object$latlon
     shp.obj = color.bind(col, pf$shape.object)
+
+    ## cols = col.fun(pf$shape.object$col.fun, pf$shape.object$col.args)
+
     
 
     ## missing data
--- _scripts/newtest.R
+++ _scripts/newtest.R
@@ -1,57 +1,32 @@
 
 ll = 'C:/Users/yeamin/Documents/GitHub/iNZightMaps/data/world.rds'
-data.1 = read.csv('C:/Users/yeamin/Desktop/Gapminder-2008.csv',skip = 1)
+data.1 = read.csv('C:/Users/yeamin/Desktop/Gapminder.csv')
 data.2 = data.1[data.1$Country %in% c('Algeria','Angola','Burkina Faso','Cameroon',
                                       'Chad','Egypt','Libya','Mauritania','Niger',
                                       'Nigeria','Sudan'),]
 
 
-data.3 = data.1[data.1$Country %in% c('New Zealand','China'),]
-obj <- iNZightShapeMap(ll, data.region = 'Country', data = data.3)
+data.3 = data.1[data.1$Country %in% c('India','Japan','New Zealand'),]
+obj <- iNZightShapeMap(ll, data.region = 'Country', data = data.2)
 
 
 plot(obj, variable = ~CO2Emissions,
+     g1 = Year,
      region = ~Country,
-     data = data.3,
-     col.method = 'heat',
+     data = data.2,
+     col.fun = 'heat',
      transform = 'linear',
+     offset = 0.2,
      full.map =F)
 
-ee = obj$each
-length(rep(1:length(ee),ee))
-
-data.1 = read.csv('C:/Users/yeamin/Desktop/bank.csv',skip = 4)
-s.set = data.1$Country.Name %in% c('China','Japan')
-ss.set = data.1$Year %in% c(1996,2000,2004,2008)
-
-data.2 = data.1[s.set,]
-data.3 = data.1[ss.set & s.set,]
-obj <- iNZightShapeMap(ll, data.region = 'Country.Name', data = data.1)
-iNZightPlot(X1960, Country.Name, data = data.1, plottype = 'shapemap',
-            plot.features =list(
-              shape.object = obj,
-              transform = "log", 
-              col.offset = 0, 
-              col = "blue",
-              col.method = 'heat',
-              na.fill = 'gray',
-              full.map = F)
-            )
-data.3$Imports
-
-
-
-obj <- iNZightShapeMap(ll, data.region = 'Country', data = data.2)
 iNZightPlot(CO2Emissions, Country, data = data.2, plottype = 'shapemap',g1 = Year,
             plot.features =list(
               shape.object = obj,
-              transform = "normal", 
-              col.offset = 0, 
-              col = "red",
-              col.method = 'hue',
-              na.fill = 'gray',
-              full.map = F)
+              transform = "linear", 
+              col.offset = 0.2, 
+              col = "blue",
+              col.method = 'heat',
+              na.fill = 'White',
+              full.map = T)
 )
 
-
-obj$col.index[136]
