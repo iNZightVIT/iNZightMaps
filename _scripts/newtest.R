0e0e99a11801c4948737a859746f47f04cba855d
jason <374567155@qq.com>
2016-01-29 14:33:40 +1300

    2

--- _scripts/newtest.R
+++ _scripts/newtest.R
@@ -1,4 +1,41 @@
 
+
+data.1 = read.csv('C:/Users/yeamin/Desktop/Gapminder-2008.csv',skip = 1)
+location = 'C:/Users/yeamin/Desktop/world/ne_110m_admin_0_countries.shp'
+shp <- readShapeSpatial(location)
+shape.obj <- shape.extract(shp)
+
+
+data.t = data.trans(data.1$Exports,transform = 'linear')
+data.o = order.match(shp[[4]],data.1$Country)
+color = col.fun(data.o,
+                color.index = shape.obj$col.index,
+                display = 'hue',col = 'red',offset = 0)
+shapeobj = color.bind(color,shape.obj)
+plot.inzshapemap(shapeobj)
+
+
+
+
+
+
+data.2 = read.csv('C:/Users/yeamin/Desktop/GeoNet_CMT_solutions.csv')
+data.2$Latitude = data.2$Latitude + 180
+
+system.time(iNZightPlot(Longitude,Latitude,data = data.2,
+                        plottype = 'map',plot.features = list(maptype = 'roadmap')))
+
+
+
+iNZightPlot(Longitude,Latitude,data = data.2,colby =dp,g1 = str,
+            plottype = 'map',plot.features = list(maptype = 'roadmap'))
+
+
+
+data("nzquakes")
+data.2 = nzquakes
+
+
 ll = 'C:/Users/yeamin/Documents/GitHub/iNZightMaps/data/world.rds'
 data.1 = read.csv('C:/Users/yeamin/Desktop/Gapminder-2008.csv',skip = 1)
 data.2 = data.1[data.1$Country %in% c('Algeria','Angola','Burkina Faso','Cameroon',
