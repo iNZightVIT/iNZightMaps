9815f7aabdf065ccbfa2f46a5a120f30809a9b8d
Daniel Barnett <16076593+dbar344@users.noreply.github.com>
2017-11-13 16:41:53 +1300

    Moving the best match function into the Maps package

--- R/inzightmap.R
+++ R/inzightmap.R
@@ -231,3 +231,44 @@ fetchMap <- function(database = "world", region = ".", crs = 4326) {
 simplifyMap <- function(map, level) {
   sf::st_simplify(map, preserveTopology = TRUE, dTolerance = 0.05)
 }
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param filename 
+##' @return 
+##' @export
+##' @author 
+retrieveMap <- function(filename) {
+    if(grepl(".rds$", filename)) {
+        readRDS(filename)
+    } else {
+        sf::st_read(filename)
+    }
+}
+        
+##' Find the pair of variables from the map and data that have the
+##' highest number of matches. This function returns in form: (data
+##' variable, map variable)
+##' @export
+findBestMatch <- function(data, map.data) {
+    ## Eliminate variables that are associated with multiple regions
+    ## in the map
+    map.data <- map.data[, !(apply(map.data, 2, anyDuplicated))]
+    
+    ## Only count the number of unique matches (prevents situations
+    ## where one particular matched region may have so many matches it
+    ## obfuscates the fact that few other regions were matched with
+    ## that variable)
+    match.sums <- apply(data, 2, function(x) {
+        apply(map.data, 2, function(y) {
+            sum(y %in% unique(x))
+        })
+    })
+    
+    ## Find indices of the best matching pair and return
+    best.match <- which(match.sums == max(match.sums), arr.ind = TRUE)[1,]
+    best.match.vars <- c(colnames(match.sums)[best.match[2]], rownames(match.sums)[best.match[1]])
+    best.match.vars
+}
