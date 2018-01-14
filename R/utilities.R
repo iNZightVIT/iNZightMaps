40dc788af77c4d405bf8e0167bb32a14135ae7e3
Daniel Barnett <16076593+dbar344@users.noreply.github.com>
2018-01-15 12:49:32 +1300

    Using vapply instead of apply + fix problems with only 1 var

--- R/utilities.R
+++ R/utilities.R
@@ -1,11 +1,11 @@
 ##' .. content for \description{} (no empty lines) ..
 ##'
 ##' .. content for \details{} ..
-##' @title 
-##' @param filename 
-##' @return 
+##' @title
+##' @param filename
+##' @return
 ##' @export
-##' @author 
+##' @author
 retrieveMap <- function(filename) {
     if(grepl(".rds$", filename)) {
         readRDS(filename)
@@ -21,21 +21,24 @@ retrieveMap <- function(filename) {
 findBestMatch <- function(data, map.data) {
     ## Eliminate variables that are associated with multiple regions
     ## in the map
-    map.data <- map.data[, !(apply(map.data, 2, anyDuplicated, incomparables = c(NA, "")))]
-    
+    map.data <- map.data[, !(apply(map.data, 2, anyDuplicated, incomparables = c(NA, ""))), drop = FALSE]
+
     ## Only count the number of unique matches (prevents situations
     ## where one particular matched region may have so many matches it
     ## obfuscates the fact that few other regions were matched with
     ## that variable)
-    match.sums <- apply(data, 2, function(x) {
-        apply(map.data, 2, function(y) {
+    match.sums <- vapply(data, function(x) {
+        vapply(map.data, function(y) {
             sum(y %in% unique(x))
-        })
-    })
-    
+        }, numeric(1))
+    }, numeric(ncol(map.data)))
+    ## print(match.sums)
+    match.sums <- matrix(match.sums, nrow = ncol(map.data), ncol = ncol(data))
+    ## match.sums <- matrix(mapply(function(x, y) sum(y %in% unique(x)), data, map.data), nrow = ncol(map.data))
+
     ## Find indices of the best matching pair and return
-    best.match <- which(match.sums == max(match.sums), arr.ind = TRUE)[1,]
-    best.match.vars <- c(colnames(match.sums)[best.match[2]], rownames(match.sums)[best.match[1]])
+    best.match <- arrayInd(which.max(match.sums), dim(match.sums))
+    best.match.vars <- c(colnames(data)[best.match[2]], colnames(map.data)[best.match[1]])
     best.match.vars
 }
 
