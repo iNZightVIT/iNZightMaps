2318d5a90d27549482773098ae7090679d4f8327
Tom Elliott <tomelliottnz@gmail.com>
2024-05-02 09:09:30 +1200

    attempt install from cran github

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,5 +1,7 @@
 # R script
 github_deps <- c(
+    "cran/maptools",
+    "cran/rgeos",
     "iNZightVIT/iNZightTools@1.13.0",
     "iNZightVIT/iNZightMR@2.2.7",
     "iNZightVIT/iNZightPlots@2.15.1"
@@ -10,25 +12,32 @@ options(
     repos = c(
         if (OS == "Linux") RSPM <- Sys.getenv("RSPM"),
         CRAN = "https://cloud.r-project.org"
-    ),
-    install.packages.compile.from.source = "never"
+    )
 )
 
-if (OS != "Linux" && !requireNamespace("XML", quietly = TRUE)) {
-    install.packages("XML", type = "binary")
+if (!requireNamespace("pak", quietly = TRUE)) {
+    install.packages("pak")
 }
 
-if (!requireNamespace("remotes", quietly = TRUE)) {
-    install.packages("remotes")
-}
+pak::pkg_install(github_deps, dependencies = TRUE)
+pak::local_install_dev_deps(upgrade = FALSE, dependencies = TRUE)
+pak::pkg_install("rcmdcheck")
 
-remotes::install_github(github_deps,
-    INSTALL_opts = c("--no-multiarch")
-)
-remotes::install_deps(
-    dependencies = TRUE,
-    INSTALL_opts = c("--no-multiarch")
-)
-remotes::install_cran("rcmdcheck",
-    INSTALL_opts = c("--no-multiarch")
-)
+# if (OS != "Linux" && !requireNamespace("XML", quietly = TRUE)) {
+#     install.packages("XML", type = "binary")
+# }
+
+# if (!requireNamespace("remotes", quietly = TRUE)) {
+#     install.packages("remotes")
+# }
+
+# remotes::install_github(github_deps,
+#     INSTALL_opts = c("--no-multiarch")
+# )
+# remotes::install_deps(
+#     dependencies = TRUE,
+#     INSTALL_opts = c("--no-multiarch")
+# )
+# remotes::install_cran("rcmdcheck",
+#     INSTALL_opts = c("--no-multiarch")
+# )
